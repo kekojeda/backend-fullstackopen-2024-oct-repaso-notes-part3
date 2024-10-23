aa801b48f43044a0dae2ad0f2bae537889f74814
kekojeda <kekojeda@gmail.com>
2024-10-23 16:18:55 -0300

    empezando con monngo

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+const Note = require('./models/note')
+
+
+
 const express = require("express");
 const cors = require('cors')
+const mongoose = require('mongoose')
 
 const app = express();
+
 
 //para hacer uso de post necesitamos json-parser de express
 app.use(express.json())
 
 app.use(cors())
 app.use(express.static('dist'))
+
+const password = process.argv[2]
 
 let notes = [
   {
@@ -27,6 +35,26 @@ let notes = [
   },
 ];
 
+const url =
+  `mongodb+srv://kekojeda:${password}@cluster0.azazoug.mongodb.net/repaso-oct-backend-notes-part3?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+const Note = mongoose.model('Note', noteSchema)
+
 const generateId = () => {
     const maxId = notes.length > 0
       ? Math.max(...notes.map(n => n.id))
@@ -39,7 +67,9 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/notes", (request, response) => {
-  response.json(notes);
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 });
 
 app.get("/api/notes/:id", (request, response) => {
