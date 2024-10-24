9337ef53aa741558265f4ee1ccc487a0b1eb3bef
kekojeda <kekojeda@gmail.com>
2024-10-24 15:59:11 -0300

    update

--- index.js
+++ index.js
@@ -8,32 +8,13 @@ const cors = require('cors')
 
 const app = express();
 
+app.use(express.static('dist'))
 
 //para hacer uso de post necesitamos json-parser de express
 app.use(express.json())
 
 app.use(cors())
-app.use(express.static('dist'))
 
-
-
-// let notes = [
-//   {
-//     id: 1,
-//     content: "HTML is easy",
-//     important: true,
-//   },
-//   {
-//     id: 2,
-//     content: "Browser can execute only JavaScript",
-//     important: false,
-//   },
-//   {
-//     id: 3,
-//     content: "GET and POST are the most important methods of HTTP protocol",
-//     important: true,
-//   },
-// ];
 
 
 app.get("/", (request, response) => {
@@ -46,18 +27,26 @@ app.get("/api/notes", (request, response) => {
   })
 });
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
+//modificando para q pase hacia adelante el error al middleware
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+      .then(note => {
+        if(note){
+          response.json(note)
+        }else{
+          response.status(400).end()
+        }
   })
+    .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-  
-    response.status(204).end()
-  })
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
   app.post('/api/notes', (request, response) => {
     const body = request.body
@@ -75,6 +64,27 @@ app.delete('/api/notes/:id', (request, response) => {
       response.json(savedNote)
     })
   })
+
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  // controlador de solicitudes con endpoint desconocido
+  app.use(unknownEndpoint)
+
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+  }
+  
+  // este debe ser el último middleware cargado, ¡también todas las rutas deben ser registrada antes que esto!
+  app.use(errorHandler)
+
 
 
 const PORT = process.env.PORT
